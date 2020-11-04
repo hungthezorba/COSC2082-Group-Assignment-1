b29da3f97200fbf2903145c3b8730b7d584d5c6c
hungnguyen <hungthezorba@gmail.com>
2020-11-04 20:52:36 +0700

    Add Mode

---
+++ mode.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Hung Nguyen on 26/04/19.
+//
+
+#include <iostream>
+#include "mode.h"
+
+using namespace std;
+
+
+int mode(int array[], int arrSize) {
+    int maxCount = 0;
+    int mode;
+    int count;
+    for (int i = 0; i < arrSize; i++) {
+        count = 0;
+        for (int j = 0; j < arrSize; j++) {
+            if (array[i] == array[j]) {
+                count++;
+            }
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            mode = array[i];
+        }
+    }
+    return mode;
+}
