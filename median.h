9b4f4980a4741ac58cd5da33aca392e9fbfde24e
unknown <datngonguyentien@gmail.com>
2020-11-06 14:26:12 +0700

    Add median

---
+++ median.h
@@ -0,0 +1,13 @@
+//
+// Created by Enterprise on 11/6/2020.
+//
+
+#ifndef MEDIAN_H
+#define MEDIAN_H
+
+void swap(int *xp, int *yp);
+void bubbleSort(int arr[], int n);
+double findMedian(int a[],int n);
+
+
+#endif //UNTITLED_MEDIAN_H
