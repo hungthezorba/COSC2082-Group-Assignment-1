5ea6a8c72a9ca3b6f5dfcfbf4963a9d23e03d16a
hungnguyen <hungthezorba@gmail.com>
2020-11-26 15:10:45 +0700

    Refactor first quartile and third quartile

--- firstQuartile.cpp
+++ firstQuartile.cpp
@@ -9,12 +9,20 @@
 using namespace std;
 
 double findFirstQuartile(int array[], int arrSize) {
-    int lowerHalf[arrSize/2]; // declare a array contains lower half in data
+    int *lowerHalf = new int[arrSize/2]; // declare a array contains lower half in data
     double firstQuartile;
-    for (int i = 0; i < arrSize/2; i++) {
-        lowerHalf[i] = array[i]; //
+    int *newArr = new int[arrSize];
+    for (int j = 0;j<arrSize;j++){
+        newArr[j] = array[j];
     }
+    bubbleSort(newArr,arrSize);
+
+    for (int i = 0; i < arrSize/2; i++) {
+        lowerHalf[i] = newArr[i]; //
+    }
+    delete newArr;
 
     firstQuartile = findMedian(lowerHalf, arrSize/2);
+    delete lowerHalf;
     return firstQuartile;
 }
--- thirdQuartile.cpp
+++ thirdQuartile.cpp
@@ -8,16 +8,26 @@ using namespace std;
 
 double findThirdQuartile(int array[], int arrSize) {
   // Declaration
-  int upperHalf[arrSize/2]; // An array with the size/2 of the input data
+  int *upperHalf = new int[arrSize/2]; // An array with the size/2 of the input data
   double thirdQuartile;
-
-  // Populate upper half array with upper half data
-  for (int i = arrSize/2 + 1; i < arrSize; i++) {
-    for (int j = 0; j < arrSize/2; j++) {
-      upperHalf[j] = array[i];
-    }
+  int index = 0;
+  int *newArr = new int[arrSize];
+  for (int j = 0;j<arrSize;j++){
+      newArr[j] = array[j];
   }
+  bubbleSort(newArr,arrSize);
 
+  if (arrSize % 2 == 0) {
+      for (int i = arrSize/2; i < arrSize; i++) {
+          upperHalf[index] = newArr[i]; //
+          index++;
+      }
+  } else {
+      for (int i = arrSize/2 + 1; i < arrSize; i++) {
+          upperHalf[index] = newArr[i]; //
+          index++;
+      }
+  }
   // Calculate third quartile with Median of upper half data
   thirdQuartile = findMedian(upperHalf, arrSize/2);
 
